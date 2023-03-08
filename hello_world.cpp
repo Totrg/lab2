3194293c91500654409fc7f33022309a40d4da82
Totrg <61617132+Totrg@users.noreply.github.com>
2023-03-08 21:51:56 +0300

    Adding dots in comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-    std::string name;	//variable for user's name
-    std::cin >> name;	//entering username
-    std::cout << "Hello world from " << name << std::endl;	//outputting username
+    std::string name;	//variable for user's name.
+    std::cin >> name;	//entering username.
+    std::cout << "Hello world from " << name << std::endl;	//outputting username.
     return 0;
 }
 
