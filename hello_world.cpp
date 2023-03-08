b6066d46ca3a9ee2b69facfebf98aa8f99f4804e
Totrg <belogolov2005@mail.ru>
2023-03-08 20:54:58 +0300

    helloworld modified 4 (Mozilla codestyle)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    std::string name;	//variable for user's name
-    std::cin >> name;	//entering username
-    std::cout << "Hello world from " << name << std::endl;	//outputting username
-    return 0;
+int
+main()
+{
+  std::string name; // variable for user's name
+  std::cin >> name; // entering username
+  std::cout << "Hello world from " << name << std::endl; // outputting username
+  return 0;
 }
-
